11e55e0772399348132a1706d53c21537ee2b2fa
Artur Losiev <losieart@cvut.cz>
2026-08-09 18:23:24 +0200

    feat: add POST endpoint and FactRequest Pydantic validation

--- server.py
+++ server.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class FactRequest(BaseModel):
+    new_fact: str
+    author: str = "Anonymous"
 
 app = FastAPI()
 
 @app.get("/facts")
 def read_facts():
-    return {"facts": ["Fact1","Fact2","Fact3"]}
+    return {"facts": ["Fact 1", "Fact 2", "Fact 3"]}
+
+@app.post("/add-fact")
+def create_fact(payload: FactRequest):
+    return {
+        "status": "success",
+        "received": payload.new_fact,
+        "author": payload.author
+    }
